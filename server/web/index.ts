47a6e7425257c20ebbd549415d8edd6fcfe4f606
lihugang <lihugang@outlook.com>
2023-01-30 11:51:52 +0800

    use eslint to format web server code

--- server/web/index.ts
+++ server/web/index.ts
@@ -24,7 +24,7 @@ app.use('/api/v1', (() => {
                 language: 'en-US',
                 editor: {
                     color: '#ff0000',
-                    fontSize: 60,
+                    fontSize: 60
                 },
                 snippets: []
             };
@@ -75,42 +75,52 @@ app.use('/api/v1', (() => {
 app.all('*', (req, res) => {
     const path = `resources${req.path}`;
     if (fs.existsSync(path) && fs.statSync(path).isFile()) {
-        const contentType = path.endsWith('.html') ? 'text/html; charset=utf-8' :
-            path.endsWith('.js') ? 'application/javascript; charset=utf-8' :
-                path.endsWith('.css') ? 'text/css; charset=utf-8' :
-                    path.endsWith('.txt') ? 'text/plain; charset=utf-8' :
-                        path.endsWith('.json') ? 'application/json; charset=utf-8' :
-                            path.endsWith('.svg') ? 'image/svg+xml' :
-                                path.endsWith('.png') ? 'image/png' :
-                                    path.endsWith('.ttf') ? 'fonts/ttf' :
-                                        path.endsWith('.pdf') ? 'application/pdf' :
-                                            'application/octet-stream';
-        if (req.headers['accept-encoding']?.includes('br'))
+        const contentType = path.endsWith('.html')
+            ? 'text/html; charset=utf-8'
+            : path.endsWith('.js')
+                ? 'application/javascript; charset=utf-8'
+                : path.endsWith('.css')
+                    ? 'text/css; charset=utf-8'
+                    : path.endsWith('.txt')
+                        ? 'text/plain; charset=utf-8'
+                        : path.endsWith('.json')
+                            ? 'application/json; charset=utf-8'
+                            : path.endsWith('.svg')
+                                ? 'image/svg+xml'
+                                : path.endsWith('.png')
+                                    ? 'image/png'
+                                    : path.endsWith('.ttf')
+                                        ? 'fonts/ttf'
+                                        : path.endsWith('.pdf')
+                                            ? 'application/pdf'
+                                            : 'application/octet-stream';
+        if (req.headers['accept-encoding']?.includes('br')) {
             if (fs.existsSync(path + '.br') && fs.statSync(path + '.br').isFile()) {
                 res.set('content-encoding', 'br').set('content-type', contentType).set('cache-control', 'public, max-age=86400');
                 fs.createReadStream(path + '.br').pipe(res);
                 return;
-            };
-        if (req.headers['accept-encoding']?.includes('gzip'))
+            }
+        }
+        if (req.headers['accept-encoding']?.includes('gzip')) {
             if (fs.existsSync(path + '.gz') && fs.statSync(path + '.gz').isFile()) {
                 res.set('content-encoding', 'gzip').set('content-type', contentType).set('cache-control', 'public, max-age=86400');
                 fs.createReadStream(path + '.gz').pipe(res);
                 return;
-            };
+            }
+        }
         res.set('content-type', contentType).set('cache-control', 'public, max-age=86400');
         fs.createReadStream(path).pipe(res);
-
     } else {
         // file not exists, fallback to index.html
         const content = fs.readFileSync('resources/index.html').toString();
         const sha256 = crypto.createHash('sha256').update(content).digest('hex');
         if (req.headers['If-None-Match'] === sha256) {
             return res.status(304).send('');
-        };
+        }
         res.set('content-type', 'text/html; charset=utf-8');
         res.set('etag', sha256);
         res.send(content);
-    };
+    }
 });
 
 app.listen(config.port);
