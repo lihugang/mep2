2d3994347460063a5923af2f089da394fb3be89d
lihugang <lihugang@outlook.com>
2023-02-07 12:36:15 +0800

    [frontend] remove unused workers for Monaco-Editor

--- src/monaco-editor-env.ts
+++ src/monaco-editor-env.ts
@@ -1,16 +1,7 @@
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 self.MonacoEnvironment = {
-    getWorker(workerID: string, label: string) {
-        return {
-            json: new jsonWorker(),
-            css: new cssWorker(),
-            html: new htmlWorker(),
-            typescript: new tsWorker(),
-            javascript: new tsWorker()
-        }[label] ?? new EditorWorker();
+    getWorker() {
+        // js/ts/css/html doesn't need support, we use our custom script language
+        return new EditorWorker();
     }
 };
